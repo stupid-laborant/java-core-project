efa464580b073c2ebb92365c6fbf8ea7fd180b98
IHaveBoughtWinRar <ivebotwinrar@mail.ru>
2022-06-03 16:04:58 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1997: \"Hello World!\"");
+        System.out.println("2022: \"Cloud Native Distributed Containerized RESTful microservice based on ARMv7 w k8s Hello World! as a service\"");
+    }
+}
